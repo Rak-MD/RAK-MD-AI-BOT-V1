760fcd56f18af4aa17fe1cf6d1c6737f4bd0c7af
ROMAN MD <romanalikazmimd@gmail.com>
2025-01-09 20:08:14 +0500

    coming soon

---
+++ language.js
@@ -0,0 +1,12 @@
+module.exports = {
+  en: {
+    welcome: 'Welcome to our bot!',
+    help: 'How can I assist you today?',
+    goodbye: 'Goodbye! See you soon!',
+  },
+  id: {
+    welcome: 'Selamat datang di bot kami!',
+    help: 'Bagaimana saya bisa membantu Anda hari ini?',
+    goodbye: 'Sampai jumpa lagi!',
+  },
+};
