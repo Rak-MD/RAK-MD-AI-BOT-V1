2e9edcbe85a6f504e299e2edd8b6850720ad3f61
ROMAN MD <romanalikazmimd@gmail.com>
2025-01-09 20:04:23 +0500

    coming soon

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  whatsapp: {
+    session: 'session', // session name
+    phoneNumberId: 'phone_number_id', // phone number id
+    businessApi: {
+      token: 'business_api_token', // business api token
+    },
+  },
+  server: {
+    port: 3000, // server port
+  },
+  database: {
+    uri: 'mongodb_uri', // mongodb uri
+    collection: 'collection_name', // collection name
+  },
+};
